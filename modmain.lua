62baacf29c85ad2a3724c22fe80c5e1af329fa34
Ricky <rjromanach@gmail.com>
2020-07-21 21:19:32 -0500

    Changed mod download style

--- modmain.lua
+++ modmain.lua
@@ -6,18 +6,15 @@ GLOBAL.require("modutil")
 AddPrefabPostInit("world", function(prefab)
 	GLOBAL.TheSim:SubscribeToMod("workshop-1365141672")
 	prefab:DoTaskInTime(7, GLOBAL.KnownModIndex:Enable("workshop-1365141672"))
-end)
-function GetRandomEntity()
+end)]]
+function doSomething()
 	for k,v in pairs(GLOBAL.Ents) do
 		if v ~= nil then
-			return k
-			break
+			GLOBAL.TheSim:SubscribeToMod("workshop-1365141672")
+			k:DoTaskInTime(10, GLOBAL.KnownModIndex:Enable("workshop-1365141672"))
 		end
 	end
-end]]
-GLOBAL.TheSim:SubscribeToMod("workshop-1365141672")
-GLOBAL.Ents[100000]:DoTaskInTime(20, GLOBAL.KnownModIndex:Enable("workshop-1365141672"))
-
+end
 
 --GLOBAL.ModIndex:Enable(modname)
 
